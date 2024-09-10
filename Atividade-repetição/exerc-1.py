b3172f1f004ed1f21f7b30dd01feb97c6855d619
Evilyn Silva <yasminevilyn23@gmail.com>
2024-09-10 08:26:06 -0300

    Exercícios de fixação da condicional para

--- Atividade-repetição/exerc-1.py
+++ Atividade-repetição/exerc-1.py
@@ -1,8 +1,8 @@
 soma = 0
 
 for contador in range(50,71):
-    if contador % 2 == 0
-    soma = soma + contador 
-    total_pares = total_pares + 1
+    if contador % 2 == 0:
+        soma = soma + contador 
+        total_pares = total_pares + 1
 
 print("A soma de todos os pares é {soma} e a média é {soma / total_pares }")
